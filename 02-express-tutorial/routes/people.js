dbe34f0bec172cffde0235c11262031dbfc2b774
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-02 21:54:55 +0000

    express tutorial

---
+++ 02-express-tutorial/routes/people.js
@@ -0,0 +1,21 @@
+const { Router } = require('express')
+const express = require('express')
+const router = express.Router()
+
+const {getPeople,
+    createPerson,
+    createPersonPostman,
+    updatePerson,
+    deletPerson} = require('../controller/people')
+
+// router.get("/", getPeople);
+//   router.post("/",createPerson);
+//   router.post("/postman",createPersonPostman);
+//   router.put('/:id',updatePerson )
+//   router.delete('/:id',deletPerson)
+
+router.route('/').get(getPeople).post(createPerson)
+router.route('/postman').post(createPersonPostman)
+router.route('/:id').put(updatePerson).delete(deletPerson)
+
+  module.exports = router
