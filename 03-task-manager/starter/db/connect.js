2d3a4ab5362ca91fb9fe90de6d114ca66d782c4e
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-03 16:32:39 +0000

    kicking up with mongoDB

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,1 +1,14 @@
-const connectionString = 'mongodb+srv://bliss:<password>@cluster0.hbla0bp.mongodb.net/?retryWrites=true&w=majority'
+const mongoose = require("mongoose");
+
+const connectionString =
+  "mongodb+srv://bliss:1738@cluster0.hbla0bp.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true,
+  })
+  .then(() => console.log("CONNECTION TO THE DB..."))
+  .catch((err) => console.log(err));
