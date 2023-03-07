6e8029ebbcb33c16926716790270759eee577ae7
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-07 14:30:41 +0000

    express crud functional for task manager app

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://bliss:1738@cluster0.hbla0bp.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
 
-mongoose
-  .connect(connectionString,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useFindAndModify:false,
-    useUnifiedTopology:true,
-  })
-  .then(() => console.log("CONNECTION TO THE DB..."))
-  .catch((err) => console.log(err));
+module.exports = connectDB;
