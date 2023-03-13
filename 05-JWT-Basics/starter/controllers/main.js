10c1b9ae1d46ed6ab8d6e5eb31c350342c65008b
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-13 15:09:46 +0000

    JWT received from frontEnd

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,15 +1,34 @@
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  // mongoose validation
+  // Joi
+  // check in controller
 
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
 
-const login = async (req,res) =>{
-    res.send('Fake Login/Register/Signup Route')
-}
+  const id = new Date().getDate();
 
-const dashboard = async (req,res) =>{
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello, Bliss Armah`,secret:`Here is your authorized data,your lucky number is ${luckyNumber}`})
-} 
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user createed", token });
+};
+
+const dashboard = async (req, res) => {
+    console.log(req.user);
+    const luckyNumber = Math.floor(Math.random() * 100);
+        res.status(200).json({
+          msg: `Hello, ${req.user.username}`,
+          secret: `Here is your authorized data,your lucky number is ${luckyNumber}`,
+        });
+};
 
 module.exports = {
-    login,
-    dashboard
-}
+  login,
+  dashboard,
+};
