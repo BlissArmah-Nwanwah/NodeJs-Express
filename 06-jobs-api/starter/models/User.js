774ffbc26e4a39692db45f990acb0720de1c9c73
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-14 14:20:44 +0000

    hashing user password with mongoose middleware

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -12,7 +13,7 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please Provide a email"],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide valid email"
+      "Please provide valid email",
     ],
     unique: true,
   },
@@ -23,4 +24,8 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 module.exports = mongoose.model("User", UserSchema);
