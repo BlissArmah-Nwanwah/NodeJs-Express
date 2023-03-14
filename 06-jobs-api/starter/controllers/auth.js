6a536a336c45d29139589d3b67d05ce7d7cf821c
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-14 14:31:11 +0000

    creating tokens for user detail on registering

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,15 +1,14 @@
-const User = require('../models/User')
-const {StatusCodes} = require('http-status-codes')
-const {BadRequestError} = require('../errors')
-const bcrypt = require('bcryptjs')
-
-
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  
-  const user = await User.create({...req.body})
-  
-  res.status(StatusCodes.CREATED).json({user})
+  const user = await User.create({ ...req.body });
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecrert", {
+    expiresIn: "30d",
+  });
+  res.status(StatusCodes.CREATED).json({user:{name:user.name}, token });
 };
 
 const login = async (req, res) => {
@@ -17,5 +16,3 @@ const login = async (req, res) => {
 };
 
 module.exports = { register, login };
-
-  
