c59a9c2c71d42e6b37cb2472d306e0615fe0dd43
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-15 08:20:44 +0000

    created job logic made

--- 06-jobs-api/starter/models/Job.js
+++ 06-jobs-api/starter/models/Job.js
@@ -13,7 +13,7 @@ const JobSchema = new mongoose.Schema({
   },
   status:{
     type:String,
-    enum:['interview','decline','pendding'],
+    enum:['interview','decline','pending'],
     default: 'pending',
   },
   createdBy:{
