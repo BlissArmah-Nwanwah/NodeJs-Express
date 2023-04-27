5a87cc9e57e505af51498d216b5e36a823d63851
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-04-27 11:02:44 +0000

    delete method worked on

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,19 +7,21 @@ app.use(express.static("./methods-public"));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 // parse json
-app.use(express.json())
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
-app.post("/api/people",(req,res)=>{
-  const {name} = req.body
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
   if (!name) {
- return   res.status(400).json({success:false, msg:'Please provide name value'})
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
   }
-  res.status(201).json({success:true,person:name})
-})
+  res.status(201).json({ success: true, person: name });
+});
 
 app.post("/login", (req, res) => {
   const { name } = req.body;
@@ -28,6 +30,37 @@ app.post("/login", (req, res) => {
   }
   res.status(401).send("Please Provide Credentials");
 });
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(400)
+      .json({ success: false, msg: `no person with id: ${id}` });
+  }
+  const newPeople = people.map((person)=>{
+    if(person.id === Number(id)){
+       person.name = name
+    }
+    return person
+  })
+  res.status(200).json({success:true,data:newPeople})
+});
+
+app.delete('/api/people/:id',(req,res) => {
+
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(400)
+      .json({ success: false, msg: `no person with id: ${req.params.id}` });
+  }
+  const newPeople = people.filter((person)=>person.id !== Number(req.params.id) )
+  res.status(200).json({success:true,data:newPeople})
+})
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000...");
