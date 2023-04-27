fa80056c94dcbc6ac52a67802b7feb22db8f1825
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-04-27 11:44:42 +0000

    routes and controllers made

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,21 +1,18 @@
-const { Router } = require('express')
-const express = require('express')
-const router = express.Router()
-
-const {getPeople,
-    createPerson,
-    createPersonPostman,
-    updatePerson,
-    deletPerson} = require('../controller/people')
+const express = require("express");
+const router = express.Router();
+const {
+  getPeople,
+  createPerson,
+  updatePerson,
+  deletPerson,
+} = require("../controller/people");
 
 // router.get("/", getPeople);
-//   router.post("/",createPerson);
-//   router.post("/postman",createPersonPostman);
-//   router.put('/:id',updatePerson )
-//   router.delete('/:id',deletPerson)
+// router.post("/", createPerson);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletPerson);
 
 router.route('/').get(getPeople).post(createPerson)
-router.route('/postman').post(createPersonPostman)
 router.route('/:id').put(updatePerson).delete(deletPerson)
 
-  module.exports = router
+module.exports = router;
