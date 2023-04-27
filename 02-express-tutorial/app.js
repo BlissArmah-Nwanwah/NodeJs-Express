20e12f465687e7a3ea6e186b93668178bb32481d
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-04-27 09:33:30 +0000

    post request method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
-const people = require('./routes/people')
-const login = require('./routes/auth')
-// static assets
-app.use(express.static("./methods-public"));
-// parse form data
-app.use(express.urlencoded({ extended: false }));
-// parse json
-app.use(express.json());
+let {people} = require('./data')
 
-app.use('/api/people',people)
-app.use('/login',login)
+// static assets
+app.use(express.static('./methods-public'))
+
+app.get('/api/people',(req,res)=>{
+  res.status(200).json({success:true,data:people})
+})
+ 
 
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000");
+  console.log("server is listening on port 5000..."); 
 });
--- 02-express-tutorial/authorize.js
+++ 02-express-tutorial/authorize.js
@@ -3,10 +3,11 @@ const authorize = (req,res,next) =>{
     if (user === 'john') {
         req.user = {name:'john',id:3}
         next()
+        
     }
     else{
         res.status(401).send('Unauthorized')
     }
 }
 
-module.exports =authorize
+module.exports = authorize
