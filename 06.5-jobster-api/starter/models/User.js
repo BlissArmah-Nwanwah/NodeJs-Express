3a768e04833fde261b9f088540f741bc58197804
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-05-03 09:08:50 +0000

    jobster user auth modified

--- 06.5-jobster-api/starter/models/User.js
+++ 06.5-jobster-api/starter/models/User.js
@@ -23,9 +23,22 @@ const UserSchema = new mongoose.Schema({
     required: [true, 'Please provide password'],
     minlength: 6,
   },
+  lastName:{
+    type:String,
+    trim:true,
+    maxlength:30,
+    default:'lastName'
+  },
+  location:{
+    type:String,
+    trim:true,
+    maxlength:30,
+    default:'my city'
+  },
 })
 
 UserSchema.pre('save', async function () {
+  if(!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
@@ -38,7 +51,7 @@ UserSchema.methods.createJWT = function () {
       expiresIn: process.env.JWT_LIFETIME,
     }
   )
-}
+} 
 
 UserSchema.methods.comparePassword = async function (canditatePassword) {
   const isMatch = await bcrypt.compare(canditatePassword, this.password)
