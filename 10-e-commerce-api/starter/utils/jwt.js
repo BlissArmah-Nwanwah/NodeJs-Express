1b22ce92d08c052332850dcb906ca9636b82c11b
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-05-16 09:17:19 +0000

    user authentication checked

--- 10-e-commerce-api/starter/utils/jwt.js
+++ 10-e-commerce-api/starter/utils/jwt.js
@@ -10,17 +10,17 @@ const createJwt = ({ payload }) => {
 
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({res,user})=>{
-  const token = createJwt({payload : user})
-  const oneDay = 1000 * 60 * 60 * 24
-  res.cookie('token',token,{
-    httpOnly:true,
-    expires:new Date(Date.now() + oneDay),
-    secure:process.env.NODE_ENV === 'production',
-    signed:true,
-  })
+const attachCookiesToResponse = ({ res, user }) => {
+  const token = createJwt({ payload: user });
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+  });
 
   // res.status(StatusCodes.CREATED).json({ user });
-}
+};
 
-module.exports = {createJwt,isTokenValid,attachCookiesToResponse}
+module.exports = { createJwt, isTokenValid, attachCookiesToResponse };
