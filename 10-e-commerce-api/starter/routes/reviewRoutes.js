5f3f1b58ac17ab4a93d4b45a9e9ee9ff7616fff9
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-05-19 09:00:58 +0000

    review controllers and routes created

--- 10-e-commerce-api/starter/routes/reviewRoutes.js
+++ 10-e-commerce-api/starter/routes/reviewRoutes.js
@@ -1,4 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const {} = require('../controller/reviewController')
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
+
+const {
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview,
+} = require("../controller/reviewController");
+
+router
+  .route("/")
+  .post([authenticateUser, authorizePermissions], createReview)
+  .get(getAllReviews);
+
+router
+  .route("/:id")
+  .get([authenticateUser, authorizePermissions], getSingleReview)
+  .patch([authenticateUser, authorizePermissions], updateReview)
+  .delete([authenticateUser, authorizePermissions], deleteReview);
+
+module.exports = router;
