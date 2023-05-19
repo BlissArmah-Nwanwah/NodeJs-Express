49849e722a0e3a32b1d77887f800bbd0e409b3e8
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-05-19 08:52:51 +0000

    review model created

--- 10-e-commerce-api/starter/controller/productController.js
+++ 10-e-commerce-api/starter/controller/productController.js
@@ -1,6 +1,8 @@
 const Product = require("../models/Products");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const path = require('path')
+
 
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId;
@@ -41,11 +43,30 @@ const deleteProduct = async (req, res) => {
   if (!product) {
     throw new CustomError.NotFoundError(`No product with id : ${productId}`);
   }
-  await product.remove()
-  res.status(StatusCodes.OK).json({msg:'Success! Product removed'})
+  await product.remove();
+  res.status(StatusCodes.OK).json({ msg: "Success! Product removed" });
 };
 const uploadImage = async (req, res) => {
-  res.send("Image upload");
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No File Uploaded");
+  }
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please Upload Image");
+  }
+  const maxSize = 1024 * 1024; 
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please Upload Image smaller than 1MB"
+    );
+  }
+
+  const imagePath = path.join(__dirname,'../public/uploads/' + `${productImage.name}`)
+
+  await productImage.mv(imagePath)
+  res.status(StatusCodes.OK).json({image: `/upload/${productImage.name}`})
 };
 
 module.exports = {
--- 10-e-commerce-api/starter/models/Products.js
+++ 10-e-commerce-api/starter/models/Products.js
@@ -20,7 +20,7 @@ const ProductSchema = new mongoose.Schema(
     },
     image: {
       type: String,
-      default: "/upload/example.jpeg",
+      default: "/upload/example.jpg",
     },
     category: {
       type: String,
