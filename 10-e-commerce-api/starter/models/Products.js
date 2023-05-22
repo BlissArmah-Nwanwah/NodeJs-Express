5c2c1ba31475cccdd8f17b386bcc157a0edcb3ac
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-05-22 09:32:06 +0000

    aggregation for average ratings and number of reviews implemented

--- 10-e-commerce-api/starter/models/Products.js
+++ 10-e-commerce-api/starter/models/Products.js
@@ -34,7 +34,7 @@ const ProductSchema = new mongoose.Schema(
     },
     company: {
       type: [String],
-      default:['#222'],
+      default: ["#222"],
       required: true,
     },
     featured: {
@@ -54,24 +54,28 @@ const ProductSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
     user: {
       type: mongoose.Types.ObjectId,
       ref: "User",
       required: true,
     },
   },
-  { timestamps: true, toJSON:{virtuals:true},toObject:{virtuals:true} }
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
-ProductSchema.virtual('reviews',{
-  ref:'Review',
-  localField:'_id',
-  foreignField:'product',
-  justOne:false, 
-})
+ProductSchema.virtual("reviews", {
+  ref: "Review",
+  localField: "_id",
+  foreignField: "product",
+  justOne: false,
+});
 
-ProductSchema.pre('remove',async function(next){
-  await this.model('Review').deleteMany({product:this._id})
-})
+ProductSchema.pre("remove", async function (next) {
+  await this.model("Review").deleteMany({ product: this._id });
+});
 
 module.exports = mongoose.model("Product", ProductSchema);
