f0622d89bc7081c6b96b2d4308e9d1969ebd9e01
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-06-06 10:05:34 +0000

    auth control work perfectly

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -7,6 +7,7 @@ const {
   createTokenUser,
   sendVerificationEmail,
   sendResetPasswordEmail,
+  createHash,
 } = require("../utils");
 const crypto = require("crypto");
 
@@ -148,7 +149,7 @@ const forgotPassword = async (req, res) => {
     const tenMinutes = 1000 * 60 * 10;
     const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
 
-    user.passwordToken = passwordToken;
+    user.passwordToken = createHash(passwordToken) ;
     user.passwordTokenExpirationDate = passwordTokenExpirationDate;
     await user.save();
   }
@@ -167,7 +168,7 @@ const resetPassword = async (req, res) => {
 
   if(user){
     const currentDate = new Date()
-    if(user.passwordToken === token && user.passwordTokenExpirationDate > currentDate){
+    if(user.passwordToken === createHash(token)  && user.passwordTokenExpirationDate > currentDate){
       user.password = password
       user.passwordToken = null;
       user.passwordTokenExpirationDate = null
